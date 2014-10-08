0e64f1c8ec720df88751527de7b6adece8b8acdd
Albert Yeung <alb.yeung@gmail.com>
2014-10-08 12:55:02 -0400

    more stuff

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def page:
-    return render_template("")
+def page():
+    return "hi"
+    return render_template("home.html")
+
+@app.route("/data")
+def data():
+    return "this is the data page"
+
+if __name__=="__main__":
+    app.debug=True
+    app.run()
